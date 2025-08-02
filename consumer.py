57218aacd80cac425869a49f779bd0037f200ebc
daeroxat <133991694+dariovolpedev@users.noreply.github.com>
2025-08-02 11:27:41 +0200

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -1,11 +1,12 @@
 import asyncio
 from middleware import Middleware
 from config import QUEUE_NAMES
+from logger import setup_logger
 
 async def consume(middleware: Middleware):
     while True:
         for qname in QUEUE_NAMES:
             message = await middleware.dispatch_message(qname)
             if message:
-                print(f"[CONSUMER - {qname.upper()}] Messaggio ricevuto: {message}")
+                logger.info(f"[{qname.upper()}] Messaggio ricevuto: {message}")
         await asyncio.sleep(0.5)
